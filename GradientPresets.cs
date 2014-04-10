31b710ba56cc6b404edce40c1ace64aa76a152a2
Teddy Bradford <teddy@teddybradford.com>
2014-04-10 13:07:28 -0400

    Fix RGB gradient and remove "island" gradient

--- GradientPresets.cs
+++ GradientPresets.cs
@@ -16,7 +16,6 @@ namespace LibNoise.Unity
         private static Gradient _grayscale;
         private static Gradient _rgb;
         private static Gradient _terrain;
-        private static Gradient _island;
 
         #endregion
 
@@ -27,22 +26,26 @@ namespace LibNoise.Unity
         /// </summary>
         static GradientPresets()
         {
+            _empty = new Gradient();
+
             List<GradientAlphaKey> alphaKeys = new List<GradientAlphaKey>();
             alphaKeys.Add(new GradientAlphaKey(1, 0));
             alphaKeys.Add(new GradientAlphaKey(1, 1));
 
-            List<GradientColorKey> islandKeys = new List<GradientColorKey>();
-            islandKeys.Add(new GradientColorKey(new Color(  3/255f,  29/255f,  63/255f), 0));
-            islandKeys.Add(new GradientColorKey(new Color(  7/255f, 106/255f, 127/255f), 0.250f));
-            islandKeys.Add(new GradientColorKey(new Color( 62/255f,  86/255f,  30/255f), 0.375f));
-            islandKeys.Add(new GradientColorKey(new Color( 84/255f,  96/255f,  50/255f), 0.500f));
-            islandKeys.Add(new GradientColorKey(new Color(130/255f, 127/255f,  97/255f), 0.625f));
-            islandKeys.Add(new GradientColorKey(new Color(184/255f, 163/255f, 141/255f), 0.750f));
-            islandKeys.Add(new GradientColorKey(new Color(212/255f, 202/255f, 204/255f), 0.875f));
-            islandKeys.Add(new GradientColorKey(new Color(234/255f, 232/255f, 236/255f), 1));
+            List<GradientColorKey> grayscaleKeys = new List<GradientColorKey>();
+            grayscaleKeys.Add(new GradientColorKey(Color.black, 0));
+            grayscaleKeys.Add(new GradientColorKey(Color.white, 1));
 
-            _island = new Gradient();
-            _island.SetKeys(islandKeys.ToArray(), alphaKeys.ToArray());
+            _grayscale = new Gradient();
+            _grayscale.SetKeys(grayscaleKeys.ToArray(), alphaKeys.ToArray());
+
+            List<GradientColorKey> rgbKeys = new List<GradientColorKey>();
+            rgbKeys.Add(new GradientColorKey(new Color(1, 0, 0), 0));
+            rgbKeys.Add(new GradientColorKey(new Color(0, 1, 0), 0.5f));
+            rgbKeys.Add(new GradientColorKey(new Color(0, 0, 1), 1));
+
+            _rgb = new Gradient();
+            _rgb.SetKeys(rgbKeys.ToArray(), alphaKeys.ToArray());
 
             List<GradientColorKey> terrainKeys = new List<GradientColorKey>();
             terrainKeys.Add(new GradientColorKey(new Color(0, 0, 0.5f), 0));
@@ -56,23 +59,6 @@ namespace LibNoise.Unity
 
             _terrain = new Gradient();
             _terrain.SetKeys(terrainKeys.ToArray(), alphaKeys.ToArray());
-
-            List<GradientColorKey> rgbKeys = new List<GradientColorKey>();
-            rgbKeys.Add(new GradientColorKey(new Color(0, 0, 1), 0));
-            rgbKeys.Add(new GradientColorKey(new Color(0, 1, 0), 0.5f));
-            rgbKeys.Add(new GradientColorKey(new Color(1, 0, 0), 1));
-
-            _rgb = new Gradient();
-            _rgb.SetKeys(rgbKeys.ToArray(), alphaKeys.ToArray());
-
-            List<GradientColorKey> grayscaleKeys = new List<GradientColorKey>();
-            grayscaleKeys.Add(new GradientColorKey(Color.black, 0));
-            grayscaleKeys.Add(new GradientColorKey(Color.white, 1));
-
-            _grayscale = new Gradient();
-            _grayscale.SetKeys(grayscaleKeys.ToArray(), alphaKeys.ToArray());
-
-            _empty = new Gradient();
         }
 
         #endregion
@@ -109,14 +95,6 @@ namespace LibNoise.Unity
         public static Gradient Terrain
         {
             get { return _terrain; }
-        }
-
-        /// <summary>
-        /// Gets the island instance of Gradient.
-        /// </summary>
-        public static Gradient Island
-        {
-            get { return _island; }
         }
 
         #endregion
